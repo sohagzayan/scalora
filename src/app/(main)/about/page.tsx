ca0aaab38e8bb3c422c074eaf9fe378a1158bae3
Sohag Hossain <sohaghossain@Sohags-MacBook-Air.local>
2025-02-15 16:53:40 +0600

    remove contact

---
+++ src/app/(main)/about/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from '@/components/common/ComingSoon/ComingSoon'
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <ComingSoon />
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ src/app/(main)/doc/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from '@/components/common/ComingSoon/ComingSoon'
+import React from 'react'
+
+const DocPage = () => {
+    return (
+        <div>
+            <ComingSoon />
+        </div>
+    )
+}
+
+export default DocPage
